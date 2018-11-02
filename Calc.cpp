3c2c1accf9c85b495836abe56d8fb360edf9e7c4
yung byun <yungcheolbyun@gmail.com>
2018-11-02 10:16:00 +0900

    asfd

--- Calc.cpp
+++ Calc.cpp
@@ -3,25 +3,23 @@
 #include <stdlib.h>
 #include <windows.h>
 
-/*
-RunCalculator 함수를 호출하면됨.
-*/
-void DisplayMenu() {
-	printf("메뉴를 선택하세요.\n");
-	printf("============\n");
+void MenuDisplay() {
+	printf("다음 메뉴 중 하나를 선택하세요.\n");
+	printf("-------------------------------\n");
 	printf("1. 더하기\n");
 	printf("2. 빼기\n");
 	printf("3. 곱하기\n");
 	printf("4. 나누기\n");
-	printf("q. 종료\n");
+	printf("q. 종료 \n");
 }
 void Before() {
 	system("cls");
-	printf("두 수를 입력하세요\n");
+	printf("두 수를 입력하세요.\n");
 }
 void After() {
-	printf("Press any key...");
-	_getch();
+	printf("아무 키나 누루세요");
+	getchar();
+	getchar();
 	system("cls");
 }
 void Add() {
@@ -42,16 +40,19 @@ void Mul() {
 void Div() {
 	int a, b;
 	scanf_s("%d %d", &a, &b);
-	printf("%d / %d = %f\n", a, b, a / float(b));
+	printf("%d / %d = %f\n", a, b, (double)a / b);
 }
 void ShowIntro() {
-	printf("계산기 프로그램에 오신걸 환영합니다!\n");
+	printf("계산기 프로그램 프로그램에 오신 걸 환영합니다!");
 	Sleep(3000);
 	system("cls");
 }
-void GetUserInputAndCompute() {
+void GetUserInputProcess() {
 	char ch = _getch();
+	int a = 0, b = 0;
 	switch (ch) {
+	case 'q':
+		exit(1);
 	case '1':
 		Before();
 		Add();
@@ -72,16 +73,13 @@ void GetUserInputAndCompute() {
 		Div();
 		After();
 		break;
-	case 'q':
-		exit(1);
-	case 27:
-		exit(1);
 	}
 }
-void RunCalculator() {
+void Run() {
 	ShowIntro();
+
 	while (1) {
-		DisplayMenu();
-		GetUserInputAndCompute();
+		MenuDisplay();
+		GetUserInputProcess();
 	}
 }
